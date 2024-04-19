ee9ae5ac1f6a55db77bf8a283fbbd72cd7cfd37f
Andrii Voloshchuk <andriyvoloshyk228@gmail.com>
2024-04-19 11:44:39 +0300

    refactor: added new containers && text for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import reactLogo from '@/assets/react.svg';
-
-import { HeaderComponent } from './components/header/Header.component.tsx';
+import image from '@/assets/image.svg';
 
 import './App.css';
 
 function App() {
     return (
         <>
-            <HeaderComponent />
-            <main className="home">
-                <h1>
-                    <img src={reactLogo} alt="React logo" />
-                    Home page
-                </h1>
-            </main>
+            <div className="container">
+                <img src={image} alt="Image" className="img" />
+
+                <div className="text">
+                    <main className="home">
+                        <h1>About me</h1>
+                        <p>
+                            Hi! My name is Andriy Voloshtchuk and I`m a Junior Frontend Developer. I am already familiar with main Web
+                            Technologies like React, HTML, CSS, JavaScript and Git version control system.
+                        </p>
+
+                        <p>
+                            This page was developed during the course
+                            <a href="https://www.mastersacademy.education/frontend-for-beginners-it"> `Intro to React`</a> from Masters
+                            Academy in 2024.
+                        </p>
+
+                        <p>
+                            This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to
+                            create my own small project for the portfolio.
+                        </p>
+
+                        <p>
+                            You can contact me via Telegram @Hyper22 and check out my{' '}
+                            <a href=" https://github.com/PixelsPioneer ">GitHub </a>.
+                        </p>
+                    </main>
+                </div>
+            </div>
         </>
     );
 }
