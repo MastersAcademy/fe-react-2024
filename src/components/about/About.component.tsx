c1de618c491a291cff43b407f5bd1964b99f1a00
Katya Vakulenko <rogerosha@gmail.com>
2024-04-20 20:12:47 +0200

    made mobile version of page

---
+++ src/components/about/About.component.tsx
@@ -0,0 +1,25 @@
+import styles from './about.module.css';
+
+export const AboutComponent = () => {
+    const aboutDescriptionClass = styles['about-description'];
+    return (
+        <div className={styles['about-container']}>
+            <img className={styles['about-image']} src="./about.png" alt="Me" />
+            <div className={styles['about-text']}>
+                <h2 className={styles['about-title']}>About me</h2>
+                <p className={aboutDescriptionClass}>
+                    Hi! My name is Katya and I&amp;apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
+                    React, HTML, CSS, JavaScript and Git version control system.
+                </p>
+                <p className={aboutDescriptionClass}>
+                    This page was developed during the course &amp;lsquo;Intro to React&amp;rsquo; from Masters Academy in 2024.
+                </p>
+                <p className={aboutDescriptionClass}>
+                    This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own
+                    small project for the portfolio.
+                </p>
+                <p className={aboutDescriptionClass}>You can contact me via [social network name] and check out my GitHub.</p>
+            </div>
+        </div>
+    );
+};
