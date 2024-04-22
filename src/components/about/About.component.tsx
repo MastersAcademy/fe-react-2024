304fa520445bad66cdcd066a254c0fd6c6501196
VasylBiliak <147659216+VasylBiliak@users.noreply.github.com>
2024-04-22 18:17:30 +0300

    Update About.component.tsx
    removed redundant styles.description

--- src/components/about/About.component.tsx
+++ src/components/about/About.component.tsx
@@ -7,34 +7,32 @@ export function AboutComponent() {
         <div className={styles.about}>
             <section className={styles.content}>
                 <img src={picture} alt="About" className={styles.picture} />
-                <div className={styles.content}>
-                    <h1 className={styles.title}>About me</h1>
-                    <div className={styles.description}>
-                        <p>
-                            Hi! My name is Vasyl and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies
-                            like React, HTML, CSS, JavaScript and Git version control system.
-                        </p>
-                        <p>This page was developed during the course &apos;Intro to React&apos; from Masters Academy in 2024.</p>
+                <h1 className={styles.title}>About me</h1>
+                <div className={styles.description}>
+                    <p>
+                        Hi! My name is Vasyl and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
+                        like React, HTML, CSS, JavaScript and Git version control system.
+                    </p>
+                    <p>This page was developed during the course &apos;Intro to React&apos; from Masters Academy in 2024.</p>
 
-                        <p>
-                            This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to
-                            create my own small project for the portfolio.
-                        </p>
+                    <p>
+                        This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my
+                        create my own small project for the portfolio.
+                    </p>
 
-                        <p>
-                            You can contact me via{' '}
-                            <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
-                                {' '}
-                                Linkedin
-                            </a>{' '}
-                            and check out my
-                            <a href="https://github.com/VasylBiliak" rel="noreferrer">
-                                {' '}
-                                GitHub
-                            </a>
-                            .
-                        </p>
-                    </div>
+                    <p>
+                        You can contact me via{' '}
+                        <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+                            {' '}
+                            Linkedin
+                        </a>{' '}
+                        and check out my
+                        <a href="https://github.com/VasylBiliak" rel="noreferrer">
+                            {' '}
+                            GitHub
+                        </a>
+                        .
+                    </p>
                 </div>
             </section>
         </div>
