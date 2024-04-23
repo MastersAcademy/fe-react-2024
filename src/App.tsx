75100dec8406af5823417b0fd0d1036a72e3e39d
Kuroma <arconiel456@gmai.com>
2024-04-23 15:16:45 +0300

    fix: refactor text style

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ function App() {
                 <article className="about-me-text">
                     <h1 className="about-me-header">About me </h1>
                     <p className="about-me-paragraph">
-                        Hi! My name is Vladyslav Brusentsov and I`m a Junior Frontend Developer. I am already familiar with main Web
+                        Hi! My name is Vladyslav Brusentsov and I`m a Junior Frontend Developer. I&apos;m already familiar with main Web
                         Technologies like React, HTML, CSS, JavaScript and Git version control system.
                         <br />
                         <br />
-                        This page was developed during the course
-                        <a href="https://www.mastersacademy.education/frontend-for-beginners-it"> `Intro to React` </a>
-                        from Masters Academy in 2024.
+                        This page was developed during the course &lsquo;
+                        <a href="https://www.mastersacademy.education/frontend-for-beginners-it"> Intro to React </a> &rsquo; from Masters
+                        Academy in 2024.
                         <br />
                         <br />
                         This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my
