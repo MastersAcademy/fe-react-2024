b9650d2ed88e8d679d16ea7d30c712fdd52cb4f6
root <volodymyr.kravchenko@masterofcode.com>
2024-04-24 21:16:00 +0000

    The AboutComponent used in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import reactLogo from '@/assets/react.svg';
-
-import { HeaderComponent } from './components/header/Header.component.tsx';
+import { AboutComponent } from './components/about/About.component.tsx';
 
 import './App.css';
 
 function App() {
     return (
-        <>
-            <HeaderComponent />
-            <main className="home">
-                <h1>
-                    <img src={reactLogo} alt="React logo" />
-                    Home page
-                </h1>
-            </main>
-        </>
+        <main className="home">
+            <AboutComponent />
+        </main>
     );
 }
 
