d4a60d5a0a0060dd763613734038c0838d73d16a
Trokhymets <serheyfly4410@gmail.com>
2024-04-24 23:56:41 +0300

    create about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import reactLogo from '@/assets/react.svg';
-
-import { HeaderComponent } from './components/header/Header.component.tsx';
+import { AboutMeComponent } from '@/components/about/AboutMe.component.tsx';
 
 import './App.css';
 
 function App() {
     return (
         <>
-            <HeaderComponent />
-            <main className="home">
-                <h1>
-                    <img src={reactLogo} alt="React logo" />
-                    Home page
-                </h1>
-            </main>
+            <AboutMeComponent />
         </>
     );
 }
---
+++ src/components/about/AboutMe.component.tsx
@@ -0,0 +1,28 @@
+import aboutImage from '../../assets/about.jpg';
+
+import aboutStyles from './about.module.css';
+
+export const AboutMeComponent = () => (
+    <div className={aboutStyles.container}>
+        <img className={aboutStyles.image} src={aboutImage} alt="About Image" />
+        <div className={aboutStyles.text}>
+            <h1 className={aboutStyles.title}>About Me</h1>
+            <div className={aboutStyles.body1}>
+                <p>
+                    Hi! My name is Serhii and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
+                    React, HTML, CSS, JavaScript and Git version control system.
+                </p>
+                <p>This page was developed during the course &apos;Intro to React&apos; from Masters Academy in 2024.</p>
+                <p>
+                    This page was developed during the course &apos;Intro to React&apos; from Masters Academy in 2024. This is a social
+                    project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own small project
+                    for the portfolio.
+                </p>
+                <p>
+                    You can contact me via <a href="https://www.facebook.com/profile.php?id=100011329003177">FaceBook</a> and check out my
+                    <a href="https://github.com/SerhiiTrokhymets">GitHub</a>.
+                </p>
+            </div>
+        </div>
+    </div>
+);
