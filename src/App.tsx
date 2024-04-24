ac217cb6430254f898067b740507d8df4190bdf4
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-04-24 15:46:44 +0300

    add footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import reactLogo from '@/assets/react.svg';
 
+import { FooterComponent } from './components/footer/Footer.component.tsx';
 import { HeaderComponent } from './components/header/Header.component.tsx';
 
 import './App.css';
@@ -14,6 +15,7 @@ function App() {
                     Home page
                 </h1>
             </main>
+            <FooterComponent />
         </>
     );
 }
