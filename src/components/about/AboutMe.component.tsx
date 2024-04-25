11df3b43176f7b1b652b88decdf5534b0eddc6f2
Trokhymets <serheyfly4410@gmail.com>
2024-04-25 18:56:59 +0300

    update text styles

--- src/components/about/AboutMe.component.tsx
+++ src/components/about/AboutMe.component.tsx
@@ -7,19 +7,21 @@ export const AboutMeComponent = () => (
         <img className={aboutStyles.image} src={aboutImage} alt="Space img" />
         <div className={aboutStyles.text}>
             <h1 className={aboutStyles.title}>About Me</h1>
-            <div className={aboutStyles.body1}>
+            <div className={aboutStyles.description}>
                 <p>
                     Hi! My name is Serhii and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies like
                     React, HTML, CSS, JavaScript and Git version control system.
                 </p>
-                <p>This page was developed during the course &apos;Intro to React&apos; from Masters Academy in 2024.</p>
                 <p>
-                    This page was developed during the course &apos;Intro to React&apos; from Masters Academy in 2024. This is a social
-                    project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own small project
-                    for the portfolio.
+                    This page was developed during the course <span className={aboutStyles.underline}>&apos;Intro to React&apos;</span> from
+                    Masters Academy in 2024.
                 </p>
                 <p>
-                    You can contact me via <a href="https://www.facebook.com/profile.php?id=100011329003177">FaceBook</a> and check out my
+                    This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own
+                    small project for the portfolio.
+                </p>
+                <p>
+                    You can contact me via <a href="https://www.facebook.com/profile.php?id=100011329003177">FaceBook</a> and check out my{' '}
                     <a href="https://github.com/SerhiiTrokhymets">GitHub</a>.
                 </p>
             </div>
