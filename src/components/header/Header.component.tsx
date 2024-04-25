0e114de4e96c3e256e499ee356217aa14916add6
Kuroma <arconiel456@gmai.com>
2024-04-25 16:57:21 +0300

    feat: add header component

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,2 +1,19 @@
+import burgerMenu from '../../assets/BurgerMenu.svg';
+import logoMA from '../../assets/LogoMA.svg';
+import shoppingBasketLogo from '../../assets/ShoppingBasketLogo.svg';
+
 import styles from './header.module.css';
-export const HeaderComponent = () => <header className={styles.header}>Header</header>;
+
+export const HeaderComponent = () => (
+    <header className={styles.header}>
+        <img className={styles.logoMA} src={logoMA} alt="Logo MA" />
+        <nav className={styles.siteNavigation}>
+            <button className={styles.shoppingBasket}>
+                <img className={styles.shoppingBasketLogo} src={shoppingBasketLogo} alt="Shopping Basket" />
+            </button>
+            <button className={styles.burgerMenu}>
+                <img className={styles.burgerMenu} src={burgerMenu} alt="Burger Menu" />
+            </button>
+        </nav>
+    </header>
+);
