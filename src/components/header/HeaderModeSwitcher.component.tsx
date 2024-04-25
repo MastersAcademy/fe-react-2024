4016b9be1e56adc6cf5898c6ab3f113651e99b8d
Roman Orlovskyi <roman.orlovskyi@masterofcode.com>
2024-04-25 16:59:27 +0300

    fix mobile view

--- src/components/header/HeaderModeSwitcher.component.tsx
+++ src/components/header/HeaderModeSwitcher.component.tsx
@@ -2,8 +2,10 @@ import DividerIconSVG from '@/assets/icons/header/divider.svg?react';
 import MoonIconSVG from '@/assets/icons/header/moon.svg?react';
 import SunIconSVG from '@/assets/icons/header/sun.svg?react';
 
+import styles from './header-mode-switcher.module.css';
+
 export const HeaderModeSwitcherComponent = () => (
-    <div>
+    <div className={styles.headerModeSwitcher}>
         <SunIconSVG />
         <DividerIconSVG />
         <MoonIconSVG />
