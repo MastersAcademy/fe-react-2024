b3692558e8ed9a49e2a0eef360bd43ba31d096b9
root <volodymyr.kravchenko@masterofcode.com>
2024-04-26 14:13:40 +0000

    Updated text for AboutDescriptionComponent

--- src/components/about/description/AboutDescription.component.tsx
+++ src/components/about/description/AboutDescription.component.tsx
@@ -6,10 +6,10 @@ export const AboutDescriptionComponent = () => (
     <div className={styles.box}>
         <TitleComponent titleClasses={styles.aboutDescriptionTitle}>About me</TitleComponent>
         <p>
-            Hi! My name is [Name] and I&aposm a Junior Frontend Developer. I am already familiar with main Web Technologies like React,
+            Hi! My name is [Name] and I&apos;m a Junior Frontend Developer. I am already familiar with main Web Technologies like React,
             HTML, CSS, JavaScript and Git version control system.
         </p>
-        <p>This page was developed during the course &aposIntro to React&apos from Masters Academy in 2024.</p>
+        <p>This page was developed during the course &apos;Intro to React&apos; from Masters Academy in 2024.</p>
         <p>
             This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to create my own small
             project for the portfolio.
