a114a60503704b82e472fbdde3b13fb7f8ef71a0
root <volodymyr.kravchenko@masterofcode.com>
2024-04-26 13:31:36 +0000

    TitleComponentProps interface used for TitleComponent

--- src/components/common/title/Title.component.tsx
+++ src/components/common/title/Title.component.tsx
@@ -1,1 +1,5 @@
-export const TitleComponent = ({ children, titleClasses }) => <h2 className={titleClasses}>{children}</h2>;
+import React from 'react';
+
+import type TitleComponentProps from '@/interfaces/TitleComponentProps.ts';
+
+export const TitleComponent: React.FC<TitleComponentProps> = ({ children, titleClasses }) => <h2 className={titleClasses}>{children}</h2>;
