ffaeab2ea78ad79cff91d6ba50c8244fa68e01ec
bogdandrummer <bogdandrummer64@gmail.com>
2024-04-27 11:36:56 +0300

    Header done

--- src/components/header/Header.component.tsx
+++ src/components/header/Header.component.tsx
@@ -1,5 +1,6 @@
 import headerLogo from '../../assets/header_logo.svg';
-import backet from '../../assets/icons/backet.svg';
+import basket from '../../assets/icons/basket.svg';
+import burger from '../../assets/icons/burger.svg';
 import moonIcon from '../../assets/icons/moon_icon.svg';
 import out from '../../assets/icons/out.svg';
 import signUp from '../../assets/icons/sign_up.svg';
@@ -7,14 +8,21 @@ import sunIcon from '../../assets/icons/sun_icon.svg';
 
 import styles from './header.module.css';
 
+const headerLink = 'https://www.mastersacademy.education/';
+
 export const HeaderComponent = () => (
     <header className={styles.header}>
-        <nav className={styles.header__menu}>
+        <nav className={`${styles.content} ${styles.header__menu}`}>
+            <span className={styles.burger}>
+                <img src={burger} alt="burger" />
+            </span>
             <div className={styles.left__side}>
-                <img src={headerLogo} alt="header_logo" className={styles.header__logo} />
-                <article className="header__icons-block">
+                <a href={headerLink}>
+                    <img src={headerLogo} alt="header_logo" className={styles.header__logo} />
+                </a>
+                <article className={styles.icons__block}>
                     <img src={sunIcon} alt="sun_icon" className={styles.header__icon} />
-                    <span className={styles.header__icon}></span>
+                    <span className={styles.separator}></span>
                     <img src={moonIcon} alt="moon_icon" className={styles.header__icon} />
                 </article>
             </div>
@@ -22,27 +30,33 @@ export const HeaderComponent = () => (
             <div className={styles.right__side}>
                 <ul className={styles.menu__list}>
                     <li>
-                        <a href="#">About</a>
+                        <a className={styles.menu__list} href="#">
+                            About
+                        </a>
                     </li>
                     <li>
-                        <a href="#">Products</a>
-                    </li>
-                    <li>
-                        <img src={backet} alt="backet" />
-                    </li>
-                    <li>
-                        <button className="header__button">
-                            <img src={out} alt="out" />
-                            Login
-                        </button>
-                    </li>
-                    <li>
-                        <button className="header__button">
-                            <img src={signUp} alt="signup" />
-                            Sign up
-                        </button>
+                        <a className={styles.menu__list} href="#">
+                            Products
+                        </a>
                     </li>
                 </ul>
+
+                <article className={styles.flex}>
+                    <span className={styles.basket__button}>
+                        <img src={basket} alt="basket" className={styles.header__icon} />
+                    </span>
+                    <div className={styles.buttons__block}>
+                        <button className={`${styles.header__button} ${styles.login__button}`}>
+                            <img className={styles.header__icon} src={out} alt="out" />
+                            Login
+                        </button>
+
+                        <button className={`${styles.header__button} ${styles.signup__button}`}>
+                            <img className={styles.header__icon} src={signUp} alt="signup" />
+                            Sign up
+                        </button>
+                    </div>
+                </article>
             </div>
         </nav>
     </header>
