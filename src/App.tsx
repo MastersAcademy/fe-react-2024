6eddca3338dd64c1845a270f83fadf89841fa6ac
Andrii Voloshchuk <andriyvoloshyk228@gmail.com>
2024-05-06 12:00:49 +0300

    fix: reverse h1 for span and add more <br /> after span

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import image from '@/assets/image.svg';
+import image from '@/assets/image.png';
 
 import './App.css';
 
@@ -6,28 +6,27 @@ function App() {
     return (
         <>
             <div className="container">
-                <img src={image} alt=" pictures " className="img" />
-
+                <img src={image} alt=" Orange stairs to the top " className="img" />
                 <div className="text">
                     <main className="home">
-                        <h1>About me</h1>
+                        <span>About me</span>
+                        <br />
+                        <br />
+                        <br />
                         <p>
                             Hi! My name is Andriy Voloshtchuk and I`m a Junior Frontend Developer. I am already familiar with main Web
                             Technologies like React, HTML, CSS, JavaScript and Git version control system.
-                        </p>
-
-                        <p>
+                            <br />
+                            <br />
                             This page was developed during the course
                             <a href="https://www.mastersacademy.education/frontend-for-beginners-it"> `Intro to React`</a> from Masters
                             Academy in 2024.
-                        </p>
-
-                        <p>
+                            <br />
+                            <br />
                             This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to
                             create my own small project for the portfolio.
-                        </p>
-
-                        <p>
+                            <br />
+                            <br />
                             You can contact me via Telegram @Hyper22 and check out my{' '}
                             <a href=" https://github.com/PixelsPioneer ">GitHub </a>.
                         </p>
