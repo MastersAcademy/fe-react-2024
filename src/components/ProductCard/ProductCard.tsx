8dd3639e7ed5af0adb66409e01ee2105f9e7ba26
Trokhymets <serheyfly4410@gmail.com>
2024-05-08 09:53:36 +0300

    add counter to cart

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import cartSVG from '../../assets/dark_cart_icon.svg';
 import type Product from '../api/apiInterfaces.ts';
@@ -9,15 +9,26 @@ interface ProductCardAttributes {
     product: Product;
 }
 
-const ProductCard: React.FC<ProductCardAttributes> = ({ product }) => (
-    <div className={styles.container}>
-        <img className={styles.image} src={product.images[0]} alt={product.title} />
-        <h2 className={styles.title}>{product.title}</h2>
-        <div className={styles.price_container}>
-            <h2 className={styles.price}>{product.price}₴</h2>
-            <img className={styles.cart} src={cartSVG} alt="Cart" style={{ fill: 'red' }} />
+const ProductCard: React.FC<ProductCardAttributes> = ({ product }) => {
+    const [currentCounter, setCounter] = useState(0);
+    const addToCart = () => {
+        setCounter(currentCounter + 1);
+    };
+    return (
+        <div className={styles.container}>
+            <img className={styles.image} src={product.images[0]} alt={product.title} />
+            <h2 className={styles.title}>{product.title}</h2>
+            <div className={styles.price_container}>
+                <h2 className={styles.price}>{product.price}₴</h2>
+                <button onClick={addToCart} className={styles.button}>
+                    <div className={styles.cartContainer}>
+                        <img className={styles.cart} src={cartSVG} alt="Cart" style={{ fill: 'red' }} />
+                        {currentCounter > 0 ? <span className={styles.cartCounter}>{currentCounter}</span> : ''}
+                    </div>
+                </button>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export { ProductCard };
