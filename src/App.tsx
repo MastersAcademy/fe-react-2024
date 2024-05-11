9ddcd44d7f9980572869bda31865c8fcaeae0388
Andrii Voloshchuk <andriyvoloshyk228@gmail.com>
2024-05-11 15:46:43 +0300

    feature: refactoring app.css && app.tsx for better structuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,21 @@
-import image from '@/assets/image.png';
+import { useState } from 'react';
+
+import { AboutMeComponents } from '@/components/about/About_me.component.tsx';
+import Footer from '@/components/footer/Footer.component.tsx';
+
+import { HeaderComponent } from './components/header/Header.component.tsx';
 
 import './App.css';
 
 function App() {
+    const [currentComponent, setCurrentComponent] = useState('About');
+
+    const toggleComponent = (componentName: string) => setCurrentComponent(componentName);
     return (
         <>
-            <div className="container">
-                <img src={image} alt=" Orange stairs to the top " className="img" />
-                <div className="text">
-                    <main className="home">
-                        <span>About me</span>
-                        <br />
-                        <br />
-                        <br />
-                        <p>
-                            Hi! My name is Andriy Voloshtchuk and I`m a Junior Frontend Developer. I am already familiar with main Web
-                            Technologies like React, HTML, CSS, JavaScript and Git version control system.
-                            <br />
-                            <br />
-                            This page was developed during the course
-                            <a href="https://www.mastersacademy.education/frontend-for-beginners-it"> `Intro to React`</a> from Masters
-                            Academy in 2024.
-                            <br />
-                            <br />
-                            This is a social project from MOCG company where I got an opportunity to work with Frontend mentors and to
-                            create my own small project for the portfolio.
-                            <br />
-                            <br />
-                            You can contact me via Telegram @Hyper22 and check out my{' '}
-                            <a href=" https://github.com/PixelsPioneer ">GitHub </a>.
-                        </p>
-                    </main>
-                </div>
-            </div>
+            <HeaderComponent toggleComponent={toggleComponent} />
+            {currentComponent === 'About' && <AboutMeComponents />}
+            <Footer />
         </>
     );
 }
