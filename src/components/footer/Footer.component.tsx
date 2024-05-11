e650b9f1bac2fedb396b90330ff1a046494d1100
Andrii Voloshchuk <andriyvoloshyk228@gmail.com>
2024-05-11 15:48:59 +0300

    feature: create new folder footer and create Footer.component.tsx && footer.module.css  for Footer.component.tsx and create FooterIcon.tsx for img and svg file with url

---
+++ src/components/footer/Footer.component.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import FooterIcon from './FooterIcon.tsx';
+
+import styles from './footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.main}>
+                <hr />
+                <div className={styles.icon}>
+                    <FooterIcon icon="../fe-react-2024/src/assets/facebook.png" url="https://www.facebook.com/master.of.code.team/" />
+                    <FooterIcon icon="../fe-react-2024/src/assets/in.png" url="https://www.linkedin.com/company/master-of-code/" />
+                    <FooterIcon icon="../fe-react-2024/src/assets/inst.png" url="https://www.instagram.com/master.of.code.team/" />
+                </div>
+                <div className={styles.description}>
+                    <p>
+                        {' '}
+                        {/* eslint-disable-next-line react/no-unescaped-entities */}
+                        Made with 💗 on course
+                        <a href="https://www.mastersacademy.education/frontend-for-beginners-it">
+                            {' '}
+                            {/* eslint-disable-next-line react/no-unescaped-entities */}
+                            'Intro to React'
+                        </a>{' '}
+                        from Masters Academy in 2024, by Andriy Voloshchuk
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
