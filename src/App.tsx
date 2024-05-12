ed82e8fdda41a9a9a0121760519c9d56f98de0ba
Kuroma <Arconiel456@gmail.com>
2024-05-12 08:34:10 +0300

    feat: add logics toggle for navigation menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { FooterComponent } from '@/components/footer/Footer.component.tsx';
-// import { AboutMeComponents } from '@/components/about/About-me.component.tsx';
-import { ProductsListComponent } from '@/components/productsList/product-list.component.tsx';
-import { mockData } from '@/mock-data.ts';
+import { useState } from 'react';
 
+import { AboutMeComponents } from './components/about/About-me.component.tsx';
+import { FooterComponent } from './components/footer/Footer.component.tsx';
 import { HeaderComponent } from './components/header/Header.component.tsx';
+import { ProductsListComponent } from './components/productsList/Product-list.component.tsx';
+import { mockData } from './mock-data.ts';
 
 import './App.css';
 
 function App() {
+    const [currentComponent, setCurrentComponent] = useState('About');
+
+    const navigationComponent = (componentName: string) => setCurrentComponent(componentName);
     return (
         <>
-            <HeaderComponent />
-            <ProductsListComponent products={mockData} />
-            {/* <AboutMeComponents />*/}
+            <HeaderComponent navigationComponent={navigationComponent} />
+            {currentComponent === 'About' ? <AboutMeComponents /> : <ProductsListComponent products={mockData} />}
             <FooterComponent />
         </>
     );
