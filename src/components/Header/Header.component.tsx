e5a5e9b46b3981ac76cc9a78e8efd50ee95bc6d5
VasylBiliak <https://github.com/VasylBiliak>
2024-05-20 18:38:50 +0300

    Implemented dark and light theme switching functionality. Also added logic for changing color of elements

--- src/components/Header/Header.component.tsx
+++ src/components/Header/Header.component.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { useState } from 'react';
 
 import { Component } from '@/interfaces/Component';
 import type { Product } from '@/interfaces/Product';
+import { Theme } from '@/interfaces/Theme';
 
 import cart from '../../assets/header/cart.svg';
+import darkIcon from '../../assets/header/dark_active.svg';
+import divider from '../../assets/header/divider.svg';
+import lightIcon from '../../assets/header/light_active.svg';
 import loginImg from '../../assets/header/login.svg';
 import menu from '../../assets/header/Menu_Duo_LG.svg';
-import switcherMode from '../../assets/header/mode switcher.svg';
 import userAdd from '../../assets/header/user_add.svg';
 import logo from '../../assets/logo.svg';
 
@@ -18,13 +21,45 @@ interface HeaderProps {
     cartData: Array<Product>;
 }
 
+const htmlElement = document.querySelector('html');
+const isLightTheme = window.matchMedia('(prefers-color-scheme: light)').matches;
+
 export function HeaderComponent({ onChangeComponent, activeComponent, cartData }: HeaderProps) {
+    let initialTheme: Theme;
+    if (localStorage.getItem('theme')) {
+        initialTheme = localStorage.getItem('theme') as Theme;
+    } else {
+        initialTheme = isLightTheme ? Theme.LIGHT : Theme.DARK;
+    }
+
+    const [theme, setTheme] = useState<Theme>(initialTheme);
+    htmlElement!.dataset.theme = theme;
+
+    function handleChangeTheme(themeText: Theme) {
+        setTheme(themeText);
+        localStorage.setItem('theme', themeText);
+    }
+
     return (
         <header className={styles.header}>
             <img className={styles.header__logo} src={logo} alt="logo" width="40px" height="40px" />
-            <button className={styles.wrap_toggle} title="Light">
-                <img src={switcherMode} alt="switcher Mode" className={styles.switcherMode} />
-            </button>
+            <div className={styles.themes} title="Theme switcher">
+                <button
+                    className={`${styles.themes__btn} ${theme === Theme.LIGHT ? styles.theme_active : ''}`}
+                    title="Light theme"
+                    onClick={() => handleChangeTheme(Theme.LIGHT)}
+                >
+                    <img src={lightIcon} alt="light theme" width="20px" height="20px" />
+                </button>
+                <img src={divider} alt="divider" width="20px" height="20px" />
+                <button
+                    className={`${styles.themes__btn} ${theme === Theme.DARK ? styles.theme_active : ''}`}
+                    title="Dark theme"
+                    onClick={() => handleChangeTheme(Theme.DARK)}
+                >
+                    <img src={darkIcon} alt="dark theme" width="20px" height="20px" />
+                </button>
+            </div>
             <nav className={styles.header__links}>
                 <button
                     className={activeComponent === Component.ABOUT ? styles.header__link_active : styles.header__link}
@@ -38,7 +73,7 @@ export function HeaderComponent({ onChangeComponent, activeComponent, cartData }
                 >
                     Products
                 </button>
-                <button className={`${styles.wrapper_cart} ${styles.cart__link} `} title="Cart">
+                <button className={`${styles.wrapper_cart} ${styles.cart__link}`} title="Cart">
                     <img src={cart} alt="Cart" width="24px" height="24px" />
                     {cartData.length > 0 && <span className={styles.product_quantity}>{cartData.length}</span>}
                 </button>
